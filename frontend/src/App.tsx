f58ebe5ac408684d65b841887a2b14e3590bcb13
dzikriws <dzikri.ws@gmail.com>
2025-03-18 13:00:02 +0700

    remove unused file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,11 @@ import LoginPage from "./pages/LoginPage";
 import Layout from "./components/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import NotFoundPage from "./pages/NotFoundPage";
-// import UomPage from "./pages/UomPage";
-// import SupplierPage from "./pages/SupplierPage";
-// import TransactionPage from "./pages/TransactionPage";
-// import TransactionDetailPage from "./pages/TransactionDetailPage";
-// import SupplierDetailPage from "./pages/SupplierDetailPage";
-// import UserRolePage from "./pages/UserRolePage";
-// import AddTransactionPage from "./pages/AddTransactionPage";
-
 import ProtectedRoute from "./components/ProtectedRoute";
-
 import ServiceCategoryPage from "./pages/ServiceCategoryPage";
 import DoctorPage from "./pages/DoctorPage";
 import ServicePage from "./pages/ServicePage";
+import TransactionPage from "./pages/TransactionPage";
 
 function App() {
   return (
@@ -29,21 +21,7 @@ function App() {
             <Route path="service-categories" element={<ServiceCategoryPage />} />
             <Route path="doctors" element={<DoctorPage />} />
             <Route path="services" element={<ServicePage />}/>
-            {/* <Route path="transactions" element={}/> */}
-
-
-
-
-            {/* <Route path="uoms" element={<UomPage />} />
-            <Route path="suppliers" element={<SupplierPage />} />
-            <Route path="suppliers/:id" element={<SupplierDetailPage />} />
-            <Route path="users" element={<UserRolePage />} />
-            <Route path="transactions" element={<TransactionPage />} />
-            <Route
-              path="transactions/:id"
-              element={<TransactionDetailPage />}
-            />
-            <Route path="add-transaction" element={<AddTransactionPage />} /> */}
+            <Route path="transactions" element={<TransactionPage />}/>
           </Route>
         </Route>
 
--- frontend/src/pages/TransactionPage.tsx
+++ frontend/src/pages/TransactionPage.tsx
@@ -1,61 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { getTransactions } from "../services/transactionService";
-import TransactionTable from "../components/TransactionTable";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
 const TransactionPage: React.FC = () => {
-  const [transactions, setTransactions] = useState([]);
-  const [transactionType, setTransactionType] = useState<
-    "penjualan" | "pembelian" | "semua"
-  >("semua");
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchTransactions = async () => {
-      try {
-        const data = await getTransactions(
-          transactionType === "semua" ? "" : transactionType
-        );
-        setTransactions(data);
-      } catch (error) {
-        console.error("Error fetching transactions:", error);
-      }
-    };
-
-    fetchTransactions();
-  }, [transactionType]);
-
   return (
-    <div>
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl font-bold">Transaction</h1>
-        <button
-          className="btn btn-primary"
-          onClick={() => navigate("/add-transaction")}
-        >
-          + Add Transaction
-        </button>
-      </div>
-
-      <div className="flex justify-end mb-4">
-        <label className="mr-2 text-sm">Transaction Type:</label>
-        <select
-          className="border p-1 rounded bg-base-200"
-          value={transactionType}
-          onChange={(e) =>
-            setTransactionType(
-              e.target.value as "penjualan" | "pembelian" | "semua"
-            )
-          }
-        >
-          <option value="semua">All</option>
-          <option value="penjualan">Sales</option>
-          <option value="pembelian">Purchase</option>
-        </select>
-      </div>
-
-      <TransactionTable transactions={transactions} />
+    <div className="p-6 flex flex-col items-center">
+      <h2 className="text-2xl font-bold mb-4">Transaction</h2>
+      <h4 className="text-2xl font-bold mb-4">TO-DO</h4>
     </div>
   );
 };
